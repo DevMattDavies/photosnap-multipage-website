7362e426d2482c01113d1c4114e0535cbc17d56a
Matt Davies <matt@mattdavies.dev>
2022-09-04 16:28:49 +0100

    Completed mid-section images. Responsive.

--- components/Home/MidSectionImages/MidSectionImages.jsx
+++ components/Home/MidSectionImages/MidSectionImages.jsx
@@ -1,6 +1,131 @@
+import { useContext } from "react";
+import { WidthContext } from "../../../pages/_app";
+import styles from "./MidSectionImages.module.scss";
+import Link from "next/link";
 
 function MidSectionImages() {
+  const width = useContext(WidthContext);
 
+  return (
+    <div className={styles.grid}>
+      {/* --- Mountains --- */}
+      <div className={styles.grid__item}>
+        {width <= 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/mobile/mountains.jpg"
+          ></img>
+        )}
+        {width > 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/desktop/mountains.jpg"
+          ></img>
+        )}
+        <div className={styles.grid__textContainer}>
+          <h4 className={styles.grid__title}>The Mountains</h4>
+          <p className={styles.grid__attribution}>by John Appleseed</p>
+          <hr />
+          <div className={styles.grid__linkContainer}>
+            <Link href="#">
+              <a className={styles.grid__link}>READ STORY</a>
+            </Link>
+            <img
+              className={styles["grid__arrow--white"]}
+              src="/assets/shared/desktop/arrow.svg"
+            ></img>
+          </div>
+        </div>
+      </div>
+      {/* --- Sunset --- */}
+      <div className={styles.grid__item}>
+        {width <= 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/mobile/cityscapes.jpg"
+          ></img>
+        )}
+        {width > 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/desktop/cityscapes.jpg"
+          ></img>
+        )}
+        <div className={styles.grid__textContainer}>
+          <h4 className={styles.grid__title}>Sunset Cityscapes</h4>
+          <p className={styles.grid__attribution}>by Benjamin Cruz</p>
+          <hr />
+          <div className={styles.grid__linkContainer}>
+            <Link href="#">
+              <a className={styles.grid__link}>READ STORY</a>
+            </Link>
+            <img
+              className={styles["grid__arrow--white"]}
+              src="/assets/shared/desktop/arrow.svg"
+            ></img>
+          </div>
+        </div>
+      </div>
+      {/* --- Voyage --- */}
+      <div className={styles.grid__item}>
+        {width <= 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/mobile/18-days-voyage.jpg"
+          ></img>
+        )}
+        {width > 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/desktop/18-days-voyage.jpg"
+          ></img>
+        )}
+        <div className={styles.grid__textContainer}>
+          <h4 className={styles.grid__title}>Sunset Cityscapes</h4>
+          <p className={styles.grid__attribution}>by Alexei Borodin</p>
+          <hr />
+          <div className={styles.grid__linkContainer}>
+            <Link href="#">
+              <a className={styles.grid__link}>READ STORY</a>
+            </Link>
+            <img
+              className={styles["grid__arrow--white"]}
+              src="/assets/shared/desktop/arrow.svg"
+            ></img>
+          </div>
+        </div>
+      </div>
+      {/* --- Architecturals --- */}
+      <div className={styles.grid__item}>
+        {width <= 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/mobile/architecturals.jpg"
+          ></img>
+        )}
+        {width > 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/stories/desktop/architecturals.jpg"
+          ></img>
+        )}
+        <div className={styles.grid__textContainer}>
+          <h4 className={styles.grid__title}>Architecturals</h4>
+          <p className={styles.grid__attribution}>by Samantha Brooke</p>
+          <hr />
+          <div className={styles.grid__linkContainer}>
+            <Link href="#">
+              <a className={styles.grid__link}>READ STORY</a>
+            </Link>
+            <img
+              className={styles["grid__arrow--white"]}
+              src="/assets/shared/desktop/arrow.svg"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MidSectionImages
+export default MidSectionImages;
--- components/Home/StoriesandImages/StoriesandImages.jsx
+++ components/Home/StoriesandImages/StoriesandImages.jsx
@@ -1,18 +1,38 @@
 import styles from "./StoriesandImages.module.scss";
+import { useContext } from "react";
+import { WidthContext } from "../../../pages/_app";
 
 function StoriesandImages() {
+  const width = useContext(WidthContext);
   return (
     <div className={styles.grid}>
-    {/* --- Row 1 --- */}
+      {/* --- Row 1 --- */}
       <div className={styles["grid__row-1-column-1"]}>
-        <div className={styles["grid__create-and-share-image"]}></div>
+        {width < 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/mobile/create-and-share.jpg"
+          ></img>
+        )}
+        {width > 480 && width < 1008 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/tablet/create-and-share.jpg"
+          ></img>
+        )}
+        {width >= 1008 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/desktop/create-and-share.jpg"
+          ></img>
+        )}
       </div>
       <div className={styles["grid__row-1-column-2"]}>
         <div className={styles["grid__textContainer--black"]}>
           <h1 className={styles.grid__title}>
             CREATE AND SHARE YOUR PHOTO STORIES.
           </h1>
-          <p className={styles.grid__textBody}>
+          <p className={styles["grid__textBody--grey"]}>
             Photosnap is a platform for photographers and visual storytellers.
             We make it easy to share photos, tell stories and connect with
             others.
@@ -29,12 +49,29 @@ function StoriesandImages() {
       </div>
       {/* --- Row 2 --- */}
       <div className={styles["grid__row-2-column-1"]}>
-        <div className={styles["grid__beautiful-stories-image"]}></div>
+        {width < 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/mobile/beautiful-stories.jpg"
+          ></img>
+        )}
+        {width > 480 && width < 1008 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/tablet/beautiful-stories.jpg"
+          ></img>
+        )}
+        {width >= 1008 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/desktop/beautiful-stories.jpg"
+          ></img>
+        )}
       </div>
       <div className={styles["grid__row-2-column-2"]}>
         <div className={styles["grid__textContainer--white"]}>
           <h1 className={styles.grid__title}>BEAUTIFUL STORIES EVERY TIME.</h1>
-          <p className={styles.grid__textBody}>
+          <p className={styles["grid__textBody--black"]}>
             We provide design templates to ensure your stories look terrific.
             Easily add photos, text, embed maps and media from other networks.
             Then share your story with everyone.
@@ -51,12 +88,29 @@ function StoriesandImages() {
       </div>
       {/* --- Row 3 --- */}
       <div className={styles["grid__row-3-column-1"]}>
-        <div className={styles["grid__designed-for-everyone-image"]}></div>
+        {width < 480 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/mobile/designed-for-everyone.jpg"
+          ></img>
+        )}
+        {width > 480 && width < 1008 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/tablet/designed-for-everyone.jpg"
+          ></img>
+        )}
+        {width >= 1008 && (
+          <img
+            className={styles.grid__image}
+            src="/assets/home/desktop/designed-for-everyone.jpg"
+          ></img>
+        )}{" "}
       </div>
       <div className={styles["grid__row-3-column-2"]}>
         <div className={styles["grid__textContainer--white"]}>
           <h1 className={styles.grid__title}>DESIGNED FOR EVERYONE.</h1>
-          <p className={styles.grid__textBody}>
+          <p className={styles["grid__textBody--black"]}>
             Photosnap can help you create stories that resonate with your
             audience. Our tool is designed for photographers of all levels,
             brands, businesses you name it.
