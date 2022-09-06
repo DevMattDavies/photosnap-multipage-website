e7e782b4f01d3e75c9657506c0e00aed7e4bd852
Matt Davies <matt@mattdavies.dev>
2022-09-06 11:08:12 +0100

    Footer completed for mobile layout

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,12 +2,13 @@ import { useContext } from "react";
 import { WidthContext } from "../../pages/_app.js";
 import FooterDesktop from "./Desktop/FooterDesktop";
 import FooterTablet from "./Tablet/FooterTablet.jsx";
+import FooterMobile from "./Mobile/FooterMobile.jsx";
 
 function Footer() {
   const width = useContext(WidthContext);
   return (
     <>
-      {/* {width <= 480 && <FooterMobile />} */}
+      {width <= 480 && <FooterMobile />}
       {width > 480 && width < 1008 && <FooterTablet />}
       {width >= 1008 && <FooterDesktop />}
     </>
---
+++ components/Footer/Mobile/FooterMobile.jsx
@@ -0,0 +1,32 @@
+import styles from "./FooterMobile.module.scss";
+import footerIcons from "../FooterIcons";
+
+function FooterMobile() {
+  return (
+    <div className={styles.footer}>
+      <img
+        className={styles.footer__logo}
+        src="/assets/shared/desktop/logo-white.svg"
+      ></img>
+      <div className={styles.footer__iconContainer}>{footerIcons()}</div>
+      <div className={styles.footer__linksContainer}>
+        <p className={styles.footer__link}>HOME</p>
+        <p className={styles.footer__link}>STORIES</p>
+        <p className={styles.footer__link}>FEATURES</p>
+        <p className={styles.footer__link}>PRICING</p>
+      </div>
+      <div className={styles.footer__invite}>
+        <p className={styles.footer__link}>GET AN INVITE</p>
+        <img
+          className={styles["footer__arrow--white"]}
+          src="/assets/shared/desktop/arrow.svg"
+        ></img>
+      </div>
+      <p className={styles.footer__copyright}>
+        Copyright 2022. All Rights Reserved
+      </p>
+    </div>
+  );
+}
+
+export default FooterMobile;
