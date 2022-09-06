1f6ef1c2dd6f3ada69a3583bba51007bebd362ce
Matt Davies <matt@mattdavies.dev>
2022-09-06 10:52:04 +0100

    Footer completed for tablet layout

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { WidthContext } from "../../pages/_app.js";
 import FooterDesktop from "./Desktop/FooterDesktop";
+import FooterTablet from "./Tablet/FooterTablet.jsx";
 
 function Footer() {
   const width = useContext(WidthContext);
   return (
     <>
       {/* {width <= 480 && <FooterMobile />} */}
-      {/* {width > 480 && width < 1008 && <FooterTablet />} */}
+      {width > 480 && width < 1008 && <FooterTablet />}
       {width >= 1008 && <FooterDesktop />}
     </>
   );
---
+++ components/Footer/Tablet/FooterTablet.jsx
@@ -0,0 +1,37 @@
+import styles from "./FooterTablet.module.scss";
+import footerIcons from "../FooterIcons";
+
+function FooterTablet() {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer__left}>
+        <img
+          className={styles.footer__icon}
+          src="/assets/shared/desktop/logo-white.svg"
+        ></img>
+        <div className={styles.footer__linksContainer}>
+          <p className={styles.footer__link}>HOME</p>
+          <p className={styles.footer__link}>STORIES</p>
+          <p className={styles.footer__link}>FEATURES</p>
+          <p className={styles.footer__link}>PRICING</p>
+        </div>
+        <div className={styles.footer__iconContainer}>{footerIcons()}</div>
+      </div>
+      <div className={styles.footer__right}>
+        <div className={styles.footer__invite}>
+          <p className={styles.footer__link}>GET AN INVITE</p>
+          <img
+            className={styles["footer__arrow--white"]}
+            src="/assets/shared/desktop/arrow.svg"
+            alt="arrow"
+          ></img>
+        </div>
+        <p className={styles.footer__copyright}>
+          Copyright 2022. All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FooterTablet;
